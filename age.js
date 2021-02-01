baeaa2090e65b34def5438b3d6aefcce7c717a40
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-02 04:18:09 +0600

    true/false

---
+++ age.js
@@ -0,0 +1,26 @@
+// const age = 4;
+// const age = 0;
+// const age = "mehadi";
+// const age = "";
+// let age;
+// let age=NaN;
+
+
+// Falsy
+// 0
+// ""
+// undefined
+// null
+
+// Truthy
+// 0
+// ' '
+
+
+const age = " ";
+if (age.length > 0) {
+    console.log("condition is true");
+
+} else {
+    console.log("condition is false");
+}
