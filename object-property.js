f804a98a548cc7dbc46260bcd9e20e3a3c852fac
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-02 06:11:17 +0600

    object-property.js

---
+++ object-property.js
@@ -0,0 +1,13 @@
+const students = [
+    {id: 21, name:"Omar Sunny"},
+    {id: 31, name:"Mannaaaaaaaa"},
+    {id: 41, name:"Moyouri"},
+    {id: 71, name:"Deepjob"},
+    {id: 12, name:"Mehdi"},
+]
+
+const names = students.map(s => s.name);
+const ids = students.map(s => s.id);
+const bigger = students.filter(s => s.id>20);
+const biggerOne = students.find(s => s.id>20);
+console.log(biggerOne);
