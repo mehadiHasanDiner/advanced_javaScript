4be4416de238de330dfc607a0887bd8c11721ffd
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-02 20:35:19 +0600

    scope.js

--- scope.js
+++ scope.js
@@ -1,6 +1,21 @@
-function sum(first, second){
-    let result = first+second;
-    return result;
-}
 
-const 
+let bonus = 20;
+
+
+function sum(first, second) {
+    let result = first + second + bonus;
+    // console.log(result);
+    if (result > 9){
+        const mood = "happy";
+    }
+        return result;
+
+}
+// var day = "firday"
+// console.log(day);
+
+const output = sum(3, 7);
+// console.log(mood);
+
+// console.log(output);
+// console.log(bonus);
