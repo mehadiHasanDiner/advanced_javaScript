55710878b2f7d51d67f12e8cd3cdbc904b5a6e40
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-02 22:31:05 +0600

    break.js

---
+++ array-slice.js
@@ -0,0 +1,11 @@
+const nums = [1,2,3,4,5,6,7,8,9];
+// const part = nums.slice(2, 5);
+
+const removed = nums.splice(2,5);
+
+// console.log(removed);
+// console.log(part);
+const together = nums.join("ami ");
+// console.log(nums);
+console.log(together);
+
